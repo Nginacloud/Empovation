2c323b609dc054479d93ff222f44599e68245cce
Dorcas Ngina <120751714+Nginacloud@users.noreply.github.com>
2024-07-03 02:03:48 +0300

    2

--- Batch_2.sql
+++ Batch_2.sql
@@ -5,10 +5,12 @@ CREATE TABLE `data_dictionary` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- 1. average price of products in a category
-select c.`Category`, avg(p.`Unit Price USD`) as AvgUnitprice
+select p.`CategoryKey`, c.`Category`, avg(p.`Unit Price USD`) as AvgUnitprice
 from Products p
-join Categories c on p.`SubcategoryKey` = c.`SubcategoryKey`
-group by c.`Category`;
+join Categories c on p.`CategoryKey` = c.`CategoryKey`
+group by p.`CategoryKey`, c.`Category`;
+
+select * from data_dictionary;
 
 -- 2. Customer purchases by gender
 select c.`Gender`, count(distinct s.`Order Number`) as NumberofOrders
@@ -23,7 +25,9 @@ left join Sales s on p.`ProductKey` = s.`ProductKey` -- no corresponding entry
 where s.`ProductKey` is null;
 
 -- 4. Currency conversion for Orders
-select s.`Order Number`, round(sum(s.`Quantity` * p.`Unit Price USD` * er.`Exchange`), 2) as TotalUSD
+select 
+  s.`Order Number`, 
+  round(sum(s.`Quantity` * p.`Unit Price USD` * er.`Exchange`), 2) as TotalUSD
 from Sales s
 join Products p on s.`ProductKey` = p.`ProductKey`
 join Exchange_Rates er on s.`Currency Code` = er.`Currency`
@@ -34,4 +38,4 @@ select * from exchange_rates;
 drop table products;
 drop table pproducts;
 
-
+select * from data_dictionary;
